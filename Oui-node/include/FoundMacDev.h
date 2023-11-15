fbee586351b983efa788497e3ff5a7f491b322bb
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2023-11-15 09:42:45 +0000

    Create FoundMacDev.h

---
+++ Oui-node/include/FoundMacDev.h
@@ -0,0 +1,6 @@
+#include "../../StampS3-Master-node/include/Cluster.h"
+bool FoundMacDev(char *);
+extern int oui_count;
+extern char oui_array[][50];
+extern char MasterSentMsg[MAX_I2C_BUFF_SIZE];
+extern char MasterReceiveMsg[MAX_I2C_BUFF_SIZE];
